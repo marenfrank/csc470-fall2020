1536d328f72fdffc9486d97fb7cacb089a894530
marenvalenti <mv7684a@student.american.edu>
2020-09-17 14:15:19 -0400

    lots of things

--- exercises/game02/Assets/Scripts/PlayerController.cs
+++ exercises/game02/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float movementSpeed = 10;
+    float rotateSpeed = 120f;
+    private int count;
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        count = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float hAxis = Input.GetAxis("Horizontal");
+        transform.Rotate(0, hAxis * rotateSpeed * Time.deltaTime, 0, Space.World);
+
+        //jump!
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y++;
+            this.transform.position = position;
+        }
         
+        if(Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+        }
+
+
+
+
+
+}
+
+        void OnTriggerEnter(Collider other)
+        {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+            count += 1;
+        }
+
     }
 }
---
+++ exercises/game02/Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public GameObject coins; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        coins = GameObject.Find("Coins");  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coins.transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime);
+    }
+}
