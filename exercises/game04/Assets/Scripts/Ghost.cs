9b9889dfb9740982b844fefbd4cee328f1e20f22
marenvalenti <mv7684a@student.american.edu>
2020-10-16 17:16:19 -0400

    mostlydone

---
+++ exercises/game04/Assets/Scripts/Ghost.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    GameObject witch;
+    public bool chase = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        witch = GameObject.Find("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (chase)
+        {
+            Vector3 directionToPlayer = witch.transform.position - transform.position;
+            directionToPlayer = directionToPlayer.normalized;
+
+
+            transform.Translate(directionToPlayer * Time.deltaTime * 6);
+        }
+       
+    }
+}
--- exercises/game04/Assets/Scripts/WitchController.cs
+++ exercises/game04/Assets/Scripts/WitchController.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class WitchController : MonoBehaviour
 {
     public float movementSpeed = 10;
     float rotateSpeed = 120f;
     private Rigidbody rb;
-    public GameObject Rocks;
     GameObject forest;
+    public GameObject GhostPrefab;
+
+    private int lives = 3;
+    public Text livesText;
+    public GameObject LoseTextObject;
+    public GameObject WinTextObject;
 
 
     // Start is called before the first frame update
@@ -17,6 +24,25 @@ public class WitchController : MonoBehaviour
         forest = GameObject.Find("Forest");
         rb = GetComponent<Rigidbody>();
 
+        SetLives();
+        LoseTextObject.SetActive(false);
+        WinTextObject.SetActive(false);
+
+
+        for (int i = 0; i < 5; i++)
+        {
+            // Choose a random position over the terrain
+            float x = Random.Range(Terrain.activeTerrain.transform.position.x, Terrain.activeTerrain.transform.position.x + Terrain.activeTerrain.terrainData.size.x);
+            float z = Random.Range(Terrain.activeTerrain.transform.position.z, Terrain.activeTerrain.transform.position.z + Terrain.activeTerrain.terrainData.size.z);
+            Vector3 pos = new Vector3(x, 0, z);
+            // Use sample height at that position to find out what y should be.
+            float y = Terrain.activeTerrain.SampleHeight(pos);
+            pos.y = y;
+            // Instantiate the tree at that position.
+            GameObject Ghost = Instantiate(GhostPrefab, pos, Quaternion.identity);
+
+
+        }
 
     }
 
@@ -29,11 +55,7 @@ public class WitchController : MonoBehaviour
         //jump!
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            //random stars when you jump
-            Vector3 pos = new Vector3(forest.transform.position.x + Random.Range(-10, 10)
-                                    , forest.transform.position.y + 3,
-                                    forest.transform.position.z + Random.Range(-10, 10));
-            Instantiate(Rocks, pos, Quaternion.identity);
+
             Vector3 position = this.transform.position;
             position.y++;
             this.transform.position = position;
@@ -49,5 +71,37 @@ public class WitchController : MonoBehaviour
 
 
 
+    }
+
+    public void SetLives()
+    {
+        livesText.text = "Lives - " + lives.ToString();
+
+        if (lives < 0)
+        {
+            LoseTextObject.SetActive(true);
+
+            SceneManager.LoadScene("level2");
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("ghost"))
+        {
+
+            Destroy(other.gameObject);
+            lives = -1;
+            SetLives();
+        }
+
+        if (other.CompareTag("bone"))
+        {
+            WinTextObject.SetActive(true);
+            Destroy(other.gameObject);
+            SceneManager.LoadScene("StartScene");
+
+        }
     }
 }
