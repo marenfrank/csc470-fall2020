562049997d0c59d1ab8767c5d9e2c58adb600810
marenvalenti <mv7684a@student.american.edu>
2020-11-07 17:06:44 -0500

    home screen

---
+++ exercises/game05/Assets/Scripts/Opening.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Opening : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnMouseClick()
+    {
+        SceneManager.LoadScene("game05");
+    }
+}
